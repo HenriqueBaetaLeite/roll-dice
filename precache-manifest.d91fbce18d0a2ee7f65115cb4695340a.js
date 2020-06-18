802a6803598fe95c9050d5fc0bac30f6fc2ba31e
henrique <hbaetaleite@gmail.com>
2020-06-18 12:41:27 -0300

    Updates

---
+++ precache-manifest.d91fbce18d0a2ee7f65115cb4695340a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967fe077289e31c15ccab09aff088234",
+    "url": "/roll-dice/index.html"
+  },
+  {
+    "revision": "2809d63604fb3fb4364d",
+    "url": "/roll-dice/static/css/main.87510494.chunk.css"
+  },
+  {
+    "revision": "2b57954b023284418506",
+    "url": "/roll-dice/static/js/2.633b68ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roll-dice/static/js/2.633b68ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2809d63604fb3fb4364d",
+    "url": "/roll-dice/static/js/main.ccb35adf.chunk.js"
+  },
+  {
+    "revision": "fc8621c203151ba7be01",
+    "url": "/roll-dice/static/js/runtime-main.41b538a6.js"
+  },
+  {
+    "revision": "619432a60a8538337191c228863e9bb5",
+    "url": "/roll-dice/static/media/d20.619432a6.gif"
+  }
+]);
