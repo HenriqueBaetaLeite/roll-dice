a3a519c6dbeb229397b97cce20d2f69edd8d44a2
henrique <hbaetaleite@gmail.com>
2020-07-23 15:37:11 -0300

    refatorando pra Hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import InputChoose from './InputChoose';
+import { InputChoose } from './InputChoose';
 import Footer from './Footer';
 
 const App = () => (
--- src/InputChoose.jsx
+++ src/InputChoose.jsx
@@ -1,45 +1,32 @@
-import React from 'react';
-import { Component } from 'react';
+import React, { useState } from 'react';
 import TheDice from './TheDice';
 import './InputChoose.css';
 
-export default class InputChoose extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      diceRolled: 6,
-    };
+const sides = [3, 4, 6, 8, 10, 12, 20, 100];
 
-    this.changeInput = this.changeInput.bind(this);
-  }
+export const InputChoose = () => {
+  const [dice, setDice] = useState(3);
 
-  changeInput(e) {
-    this.setState({ diceRolled: Number(e.target.value) });
-  }
-
-  render() {
-    const { diceRolled } = this.state;
-    return (
-      <div className="dice-container">
-        <form className="form">
-          <label className="labelDice" htmlFor="diceSide">
-            Choose the dice
-          </label>
-          <select className="selectInput" onChange={this.changeInput} name="diceSide" id="diceSide">
-            <option value="3">D3</option>
-            <option value="4">D4</option>
-            <option selected="selected" value="6">
-              D6
+  return (
+    <div className="dice-container">
+      <form className="form">
+        <label className="labelDice" htmlFor="diceSide">
+          Choose the dice
+        </label>
+        <select
+          className="selectInput"
+          onChange={(e) => setDice(Number(e.target.value))}
+          name="diceSide"
+          id="diceSide"
+        >
+          {sides.map((side) => (
+            <option key={side} value={side}>
+              D{side}
             </option>
-            <option value="8">D8</option>
-            <option value="10">D10</option>
-            <option value="12">D12</option>
-            <option value="20">D20</option>
-            <option value="100">D100</option>
-          </select>
-        </form>
-        <TheDice diceSide={diceRolled} className="dice" />
-      </div>
-    );
-  }
-}
+          ))}
+        </select>
+      </form>
+      <TheDice diceSide={dice} className="dice" />
+    </div>
+  );
+};
